66b3d20ccb4b1cb8354d061a3e525ce3311b3897
Nahuel Santos <santosnahuel98@gmail.com>
2024-09-26 17:13:07 -0300

    Subiendo contact page terminado

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,24 @@
 import ContactForm from "../components/contactFormComponent/ContactForm"
-
+import style from './pagesStyle/contactPage.module.css'
+import FotoContacto from '../utils/img/FotoContacto.jpeg'
+import { useContext } from "react"
+import { LanguageContext } from "../context/languageContext"
 
 const ContactPage = () => {
+  const { state } = useContext(LanguageContext)
+  const isEnglish = state.language
   return (
-    <div>
-      <ContactForm/>
+    <div className={style.contactPageContainer}>
+      <div className={style.imageAndTextContainer}>
+        <img src={FotoContacto} alt="Foto Sabrina Sust" className={style.imageContact} />
+        <div className={style.textContainerContact}>
+          <h3>Contactate conmigo</h3>
+          <p>{isEnglish ? 'After completing the form below, you will contact me and I would respond to you' : 'Al llenar el formulario vas a poder comunicarte conmigo y te respondere'} </p>
+        </div>
+      </div>
+
+
+      <ContactForm />
     </div>
   )
 }
