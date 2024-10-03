fd87d88f1de9b9ab79ddee5f29d4048f40cf4e3a
Nahuel Santos <santosnahuel98@gmail.com>
2024-10-03 01:29:09 -0300

    Subo casi todo terminado

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { LanguageContextProvider } from './context/languageContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageContextProvider>
-    <BrowserRouter basename="/Nahuel/AppReact/dist/index.html">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     </LanguageContextProvider>
   </StrictMode>
 
